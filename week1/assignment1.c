ae8c9388bc5761d2cda93b756864f979c2dd9002
sydprall <sydprall@verizon.net>
2025-08-25 17:20:29 +0000

    commit

---
+++ week1/assignment1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+    int var2 =25;
+    char var1 ="p";
+    float var3 =5.08;
+     
+    printf("My age is %d The first letter of my last name is: %c My height in feet is: %f", var1, var2, var3,);
+    
+
+}
