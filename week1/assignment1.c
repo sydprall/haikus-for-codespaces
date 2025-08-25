bb724251a46a82c09239a3653629b485907eaba1
sydprall <sydprall@verizon.net>
2025-08-25 18:59:05 +0000

    assignment 1 complete

--- week1/assignment1.c
+++ week1/assignment1.c
@@ -5,7 +5,7 @@ int main(){
     char var1 = "p";
     int var2 =25;
     float var3 = 5.08;
-    _Bool var4 = 1;
+    _Bool var4 = true;
      
     printf("The first letter of my last name is:%c My age is: %d My height in feet is: %f I am a student: %d", var1, var2, var3, var4); 
     
