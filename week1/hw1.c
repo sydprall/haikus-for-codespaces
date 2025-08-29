49b245f7157c52a08abdd7d45a4dd5a5deb8e7d2
sydprall <sydprall@verizon.net>
2025-08-29 02:16:16 +0000

    hw1.1

---
+++ week1/hw1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+    char lastInitial;
+    int age;
+    float height;
+
+    printf("Enter Last Initial");
+    getchar();
+    scanf("%c",&lastInitial);
+    printf("lastInitial");
+
+    printf("Enter Age");
+    scanf(%d,&age);
+    printf("age");
+   
+    printf("Enter height in feet");
+    scanf(%f, &height);
+    printf("age");
+
+}
