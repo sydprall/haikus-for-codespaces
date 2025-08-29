01b24c8bf01386273496d69506470fe71da9bc4a
sydprall <sydprall@verizon.net>
2025-08-29 03:11:07 +0000

    hw1.3

--- week1/hw1.c
+++ week1/hw1.c
@@ -2,14 +2,14 @@
 #include<stdbool.h>
 
 int main(){
-    char lastInitial;
-    int age=0;
-    float height=0;
-    bool isStudent=true
+    char var1="p";
+    int var2=25;
+    float var3=5.08;
+    bool var4=true;
 
     printf("Enter Last Initial, Age, Height, Are You A Student? And Press Enter");
     getchar();
-    scanf("%c %d %f %c", &lastInitial &age &height &isStudent);
-    printf("The first letter of the last name is: %c. The age is: %d. The height is: %f. Is a student? %c." lastInitial, age, height, isStudent);
+    scanf("%c %d %f %c", &var1 &var2 &var3 &var4);
+    printf("The first letter of the last name is: %c. The age is: %d. The height is: %f. Is a student? %c." var1, var2, var3, var4);
 
 }
