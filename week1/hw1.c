5630242761e5ec4cd157bc257511f4858c11a0c0
sydprall <sydprall@verizon.net>
2025-08-29 02:38:52 +0000

    hw1.2

--- week1/hw1.c
+++ week1/hw1.c
@@ -3,20 +3,13 @@
 
 int main(){
     char lastInitial;
-    int age;
-    float height;
+    int age=0;
+    float height=0;
+    bool isStudent=true
 
-    printf("Enter Last Initial");
+    printf("Enter Last Initial, Age, Height, Are You A Student? And Press Enter");
     getchar();
-    scanf("%c",&lastInitial);
-    printf("lastInitial");
-
-    printf("Enter Age");
-    scanf(%d,&age);
-    printf("age");
-   
-    printf("Enter height in feet");
-    scanf(%f, &height);
-    printf("age");
+    scanf("%c %d %f %c", &lastInitial &age &height &isStudent);
+    printf("The first letter of the last name is: %c. The age is: %d. The height is: %f. Is a student? %c." lastInitial, age, height, isStudent);
 
 }
