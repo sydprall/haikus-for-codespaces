635cf84c02c93b3fa3b184cbde1eca749be2c5f1
sydprall <sydprall@verizon.net>
2025-08-29 03:14:43 +0000

    hw1.4

--- week1/hw1.c
+++ week1/hw1.c
@@ -7,9 +7,9 @@ int main(){
     float var3=5.08;
     bool var4=true;
 
-    printf("Enter Last Initial, Age, Height, Are You A Student? And Press Enter");
+    printf("Enter Last Initial AND Age AND Height in feet AND Are you a student? and press enter");
     getchar();
     scanf("%c %d %f %c", &var1 &var2 &var3 &var4);
-    printf("The first letter of the last name is: %c. The age is: %d. The height is: %f. Is a student? %c." var1, var2, var3, var4);
+    printf("The first letter of the last name is: %c The age is: %d The height in feet is: %f Is a student? %c" var1, var2, var3, var4);
 
 }
