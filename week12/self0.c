afd4207b1ad870ffaa4f7953b47eabc9e037f151
sydprall <sydprall@verizon.net>
2025-11-08 02:29:53 +0000

    self

--- week12/self0.c
+++ week12/self0.c
@@ -2,13 +2,15 @@
 
 int main()
 {
-    int arr1 [3][2], arr2[4];
+    int arr1[3][2], arr2[4], temp=1;
 
     for (int i=0;i<3;i++)
     {
         for(int j=0;j<2;j++)
         {
-            printf("%d ", arr1[i][j]);
+           arr1[i][j]=temp;
+           temp++;
+           printf("%d ", arr1[i][j]);
         }
         printf("\n");
     }
@@ -17,9 +19,11 @@ int main()
     {
         arr2[i]= (i+1)*(i+1);
     }
-    for (int i=0;i<4;i++)
+
+    for(int i=0;i<4;i++)
     {
-        printf("%d ", arr2[4]);
+        printf(" %d",arr2[i]);
     }
+    printf("\n");
 return 0;
 }
--- week12/self2.c
+++ week12/self2.c
@@ -2,17 +2,39 @@
 
 int main()
 {
-    int arr1 [3][3], arr2[3][3];
+    int arr1[3][2], arr2[3][2], arr3[3][2], temp=1, temp2=1;
 
-    int even = 2, odd = 1;
     for (int i=0;i<3;i++)
     {
-        for(int j=0;j<3;j++)
+        for(int j=0;j<2;j++)
         {
-            even_arr[i][j] = 3*j +1;
-            odd_arr[i][j] = odd;
-            even = even+2;
-            odd = odd+2;
+           arr1[i][j]=3*temp+1;
+           temp++;
+           printf("%d ", arr1[i][j]);
         }
+        printf("\n");
     }
+
+     for (int i=0;i<3;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+           arr2[i][j]=(temp2*temp2);
+           temp2++;
+           printf("%d ", arr2[i][j]);
+        }
+        printf("\n");
+    }
+
+     for (int i=0;i<3;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+           arr3[i][j]=arr1[i][j]+arr2[i][j];
+           printf("%d ", arr3[i][j]);
+        }
+        printf("\n");
+    }
+
+return 0;
 }
